3777be7841b1c78e5792c5c19ee43e02451057e1
Emillycosta <127053028+Emillycosta@users.noreply.github.com>
2024-08-23 21:54:36 -0300

    Add files via upload

---
+++ mypage.js
@@ -0,0 +1,20 @@
+// Função para rolagem suave
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Mensagem de boas-vindas
+    alert('Bem-vindo ao site sobre Stranger Things!');
+});
